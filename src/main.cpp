14c6ce8cc428c87ee4f68109a26eddfe5a76204d
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-11-14 14:36:14 +0800

    > make 181250086 root Linux debian 4.19.0-12-amd64 #1 SMP Debian 4.19.152-1 (2020-10-18) x86_64 GNU/Linux  14:36:14 up  3:42,  2 users,  load average: 0.20, 0.10, 0.03 510d7550877ae53b2d626832e61fb62a5fbf446b

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,17 @@
 #include<fstream>
 using namespace std;
 int main(int argc,char* argv[]){
-	char data[100];
+	
 	ifstream infile;
 	infile.open("/root/Lab/src/hello.txt",ios::in);
 	if(!infile.is_open()){
 		cout<<"can't open the file!"<<endl;
 		return 0;
 	}
-	infile>>data;
-	cout<<data<<endl;
+	char ch;
+	while(!infile.eof()){
+		infile.get(ch);
+		cout<<ch;
+	}
 	infile.close();
 }
