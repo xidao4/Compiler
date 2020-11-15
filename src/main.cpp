13a8daf8ad39c8b014684d931d5b3c1f3d299db3
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-11-15 10:59:31 +0800

    > make 181250086 root Linux debian 4.19.0-12-amd64 #1 SMP Debian 4.19.152-1 (2020-10-18) x86_64 GNU/Linux  10:59:31 up  3:03,  2 users,  load average: 0.10, 0.14, 0.07 3810767758d74c9821587774fff5ef97c84db399

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string>
 using namespace std;
 extern FILE* yyin;
 extern "C"{
@@ -6,7 +7,7 @@ extern "C"{
 }
 int main(int argc,char** argv){
 	if(argc>1){
-		if(!(yyin=fopen(argv[1],"r"))){
+		if(!(yyin=fopen("/root/Lab/src/test0.cmm","r"))){
 			perror(argv[1]);
 			return 1;
 		}
