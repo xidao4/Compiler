0005b61554b48806eab006538466f19a0ed7f7ba
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-12-02 09:39:23 +0800

    > make 181250086 root Linux debian 4.19.0-12-amd64 #1 SMP Debian 4.19.152-1 (2020-10-18) x86_64 GNU/Linux  09:39:23 up 17 min,  2 users,  load average: 0.24, 0.10, 0.03 aefe0b67b12fa8198eb03ad3c8e217ec31f88c0d

--- src/main.cpp
+++ src/main.cpp
@@ -46,8 +46,9 @@ string getFloat(char* yytext){
 }
 int main(int argc,char** argv){
 	if(argc>1){
-//"/root/Lab/src/test0.cmm"
-		if(!(yyin=fopen(argv[1],"r"))){
+//	"/root/Lab/src/test0.cmm"
+//	argv[1]
+		if(!(yyin=fopen("/root/Lab/src/test0.cmm","r"))){
 			perror(argv[1]);
 			return 1;
 		}
