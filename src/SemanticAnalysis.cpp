d0d031353d57c8c78f87452340f3d3038551d1f3
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-12-26 23:04:25 +0800

    > make 181250086 root Linux debian 4.19.0-12-amd64 #1 SMP Debian 4.19.152-1 (2020-10-18) x86_64 GNU/Linux  23:04:25 up 11:27,  3 users,  load average: 0.28, 0.23, 0.14 e0db7919477f73b5cb66bf1febaf66a4b8c70e67

--- src/SemanticAnalysis.cpp
+++ src/SemanticAnalysis.cpp
@@ -610,15 +610,14 @@ Type Exp_ASSIGNOP(Node* n){
     Type left_type=Exp(n->child);
     if(left_type==NULL){
         printf("等号左边是空指针\n");
-    }else{
-        //左边是  右值
-        if(left_type->kind==Type_::BASIC||left_type->kind==Type_::FUNCTION||left_type->kind==Type_::ERROR ){
-            fprintf(stderr,"Error Type 6 at Line %d: 赋值号左边出现一个只有右值的表达式.\n",n->lineno);
-            return genErrType(6);
-        }
+    }
+    //左边是  右值
+    if(left_type->kind==Type_::BASIC||left_type->kind==Type_::FUNCTION||left_type->kind==Type_::ERROR ){
+        fprintf(stderr,"Error Type 6 at Line %d: 赋值号左边出现一个只有右值的表达式.\n",n->lineno);
+        return genErrType(6);
     }
     
-
+    
     printf("前面：等号右边\n");
     Type right_type=Exp(n->child->next_sib->next_sib);
     if(right_type==NULL){
