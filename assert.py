fa266549f8158ad64c7088a0bf229a79d9a685fe
tracer-compiler2020 <compilers@software.nju.edu.cb>
2020-12-30 12:54:00 +0800

    > make 181250086 root Linux debian 4.19.0-12-amd64 #1 SMP Debian 4.19.152-1 (2020-10-18) x86_64 GNU/Linux  12:54:00 up  3:21,  2 users,  load average: 0.47, 0.16, 0.05 6afb3d17674edf1cdd78fab484d3be62b7556614

---
+++ assert.py
@@ -0,0 +1,53 @@
+import os
+import sys
+from typing import List
+
+
+def analyzeOne():
+    with open(f'{pathBase}/{trueAnswer}') as f:
+        trueAnswerList = [i for i in f.read().strip().replace('Error Type ', 'Error type ').split('\n') if i]
+    with open(f'{pathBase}/{yourAnswer}') as f:
+        yourAnswerList = [i for i in f.read().strip().split('\n') if i]
+    trueAnswerDict = dict()
+    yourAnswerDict = dict()
+    for i in range(len(trueAnswerList)):
+        trueAnswerDict[trueAnswerList[i].split()[2] + '&' + trueAnswerList[i].split()[5]] = i
+    for i in range(len(yourAnswerList)):
+        yourAnswerDict[yourAnswerList[i].split()[2] + '&' + yourAnswerList[i].split()[5]] = i
+    extraList = [yourAnswerList[yourAnswerDict[i]] for i in yourAnswerDict if i not in trueAnswerDict]
+    missingList = [trueAnswerList[trueAnswerDict[i]] for i in trueAnswerDict if i not in yourAnswerDict]
+
+    if extraList or missingList:
+        print('\n  your extra output: ')
+        [print(i) for i in extraList]
+        print('\n  your missing output: ')
+        [print(i) for i in missingList]
+        input()
+
+
+if __name__ == "__main__":
+    pathBase = './tests/myLab/Tests_2_Advanced'
+
+    # paramList: List[str] = sys.argv
+    # if len(paramList) > 1:
+    #     li = [f'{i[i.rfind("/")+1:].replace(".cmm", "")}.cmm' for i in paramList[1:]]
+    # else:
+    #     li = [i for i in os.listdir(pathBase) if i.endswith('.cmm')]
+
+
+    li = [i.replace('.cmm', '') for i in os.listdir(pathBase) if i.endswith('.cmm')]
+    for i in li:
+        print(i)
+        trueAnswer = i + '.output'
+        yourAnswer = i + '.out.txt'
+        try:
+            analyzeOne()
+        except BaseException as e:
+            print(e)
+            print(repr(e))
+            print('error in file: ' + i)
+            print('please input \'c\' to check the details of your answer')
+            c = input()
+            if c == 'c':
+                print(open(f'{pathBase}/{yourAnswer}').read())
+                input()
